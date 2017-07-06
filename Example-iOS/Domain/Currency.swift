b6253193a4752639577b8cb4007a23184417af35
Roberto Frontado <robertofrontado@Robertos-MacBook-Pro-2.local>
2017-07-07 00:01:43 +0200

    Added Currency model

---
+++ Example-iOS/Domain/Currency.swift
@@ -0,0 +1,35 @@
+//
+//  Currency.swift
+//  Example-iOS
+//
+//  Created by Roberto Frontado on 7/6/17.
+//  Copyright © 2017 Roberto Frontado. All rights reserved.
+//
+
+import ObjectMapper
+
+enum CurrencyType: String {
+  case USD = "USD"
+  case EUR = "EUR"
+  case GBP = "GBP"
+}
+
+struct Currency: Mappable {
+
+  var type: CurrencyType?
+  var rate: Float?
+
+  init(type: CurrencyType, rate: Float) {
+    self.type = type
+    self.rate = rate
+  }
+
+  init?(map: Map) {
+
+  }
+
+  mutating func mapping(map: Map) {
+    type <- map["type"]
+    rate <- map["quotes.0"]
+  }
+}
