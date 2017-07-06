308d8f49d507bdde96b5b3cd764168e200a43fe6
Roberto Frontado <robertofrontado@Robertos-MacBook-Pro-2.local>
2017-07-06 23:36:37 +0200

    Added SplashViewController

---
+++ Example-iOS/Presentation/Splash/SplashViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SplashViewController.swift
+//  Example-iOS
+//
+//  Created by Roberto Frontado on 7/6/17.
+//  Copyright © 2017 Roberto Frontado. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+}
